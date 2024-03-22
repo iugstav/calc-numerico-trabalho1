f1a61f5950e247d23669cd03b126ab49af1e847d
Gustavo Soares <soaresgu2710@gmail.com>
2024-03-22 16:28:57 -0300

    rewritten function

--- fixed_point.py
+++ fixed_point.py
@@ -6,34 +6,39 @@ def fp_func(x):
     return np.sqrt(((0.2 * x) / np.power(math.e, x)) + (17 / 0.2))
 
 
-def fixed_point():
-    intervals = find_intervals()
-    print(f"intervalos: {intervals}\n")
+def fixed_point(a, epsilon, max_it = 100):
+    i = 0
+    # valor inicial
+    x0: float = 0
+    # valor a ser atualizado pela função
+    x1: float = 0
 
-    max_iterations = 100
-    epsilon = 1.0e-14
+    while i < max_it:
+        x1 = fp_func(x0)
 
-    for itv in intervals:
-        i = 0
-        # valor inicial
-        x0: float = itv[0]
-        # valor a ser atualizado pela função
-        x1: float = 0
+        # verificação para saber se está nos conformes
+        if np.abs(x0 - x1) < epsilon or np.abs(func(x1) < epsilon):
+            return x1, i
 
-        print(f"intervalo {itv}")
-        while np.abs(func(x0)) > epsilon and i < max_iterations:
-            x1 = fp_func(x0)
+        x0 = x1
+        i += 1
 
-            # verificação para saber se está nos conformes
-            if np.abs(x0 - x1) < epsilon:
-                break
-
-            x0 = x1
-
-            i += 1
-
-        print(f"iteration: {i} | phi(x1): {x0} | f(x1): {func(x0)}")
-        print("")
+    return None, i
 
 
-fixed_point()
+intervalos = [(-3, -2), (9, 10)]
+epsilon = 1.0e-15
+max_iter = 100
+
+# encontrando as raízes de cada intervalo
+for intervalo in intervalos:
+    print(f"Intervalo: {intervalo}")
+
+    a, _ = intervalo
+    raiz, iteracoes = fixed_point(a, epsilon, max_iter)
+
+    if raiz is not None:
+        print(f'Raiz encontrada: {raiz}')
+        print(f'Número de iterações: {iteracoes}')
+    else:
+        print("Não foi possível encontrar a raiz com a precisão especificada.")
